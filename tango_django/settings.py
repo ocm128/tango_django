e378024c809a0205ec8003daba75a0456570bac5
kalasni <kalassni@gmail.com>
2017-12-10 20:21:06 +0100

    Added UserProfile model and createad UserForm and UserProfileForm

--- tango_django/settings.py
+++ tango_django/settings.py
@@ -95,6 +95,13 @@ DATABASES = {
     }
 }
 
+# Password hashing functions
+# https://docs.djangoproject.com/en/1.11/topics/auth/passwords/#how-django-stores-passwords
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
