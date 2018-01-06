724f8a75576b61cc97e2937a4a7ca140bf88dc1f
kalasni <kalassni@gmail.com>
2018-01-06 19:57:33 +0100

    Fixed bugs in settigns

--- tango_django/settings.py
+++ tango_django/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'secretkey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1, '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
